5f1ccd56c6efccee857abee1f8972ae7c52cd1f0
elysee <mboussaemmanuelito@gmail.com>
2025-06-06 14:49:24 +0100

    feat(hero) advenced

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
-import { render } from 'preact'
-import './index.css'
-import { App } from './app.tsx'
+import { render } from "preact";
+import "./index.css";
+import { App } from "./app.tsx";
+import "./modules/header.ts";
+import "./modules/gsap.ts";
 
-render(<App />, document.getElementById('app')!)
+render(<App />, document.getElementById("app")!);
