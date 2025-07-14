ea8daeeac56178c0ccbd16ab188f4b5254f94a23
elysee <mboussaemmanuelito@gmail.com>
2025-07-14 22:28:43 +0100

    feat(pages) : add skills sections

---
+++ src/types/skills.ts
@@ -0,0 +1,16 @@
+export type Framework = {
+  name: string;
+  logo: string;
+};
+
+export type Techno = {
+  name: string;
+  logo?: string;
+  description?: string;
+  frameworks?: Framework[];
+};
+
+export type Skill = {
+  name: string;
+  techno: Techno[];
+};
