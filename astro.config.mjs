fd0c5e0bb34f59035f6d88ae25e906f84b6c5695
elysee <mboussaemmanuelito@gmail.com>
2025-07-18 21:32:15 +0100

    chore: revert to default GitHub Pages domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  site: "https://eembouz.com",
+  site: "https://elyseemb.github.io/",
   image: {
     service: passthroughImageService(),
   },
