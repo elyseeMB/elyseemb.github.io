7401c4b165022e3cf19a85c84c026707143c5d70
elysee <mboussaemmanuelito@gmail.com>
2025-07-31 19:52:00 +0100

    style(blog.eembouz.com/ui): fix size

--- apps/eembouz.com/.astro/content-modules.mjs
+++ apps/eembouz.com/.astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
 ["src/data/blog/load-balancer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fload-balancer.mdx&astroContentModuleFlag=true")],
-["src/data/blog/histoire-du-design-graphique.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fhistoire-du-design-graphique.mdx&astroContentModuleFlag=true")],
-["src/data/blog/qu-est-ce-que-design-pattern.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fqu-est-ce-que-design-pattern.mdx&astroContentModuleFlag=true")]]);
+["src/data/blog/qu-est-ce-que-design-pattern.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fqu-est-ce-que-design-pattern.mdx&astroContentModuleFlag=true")],
+["src/data/blog/histoire-du-design-graphique.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fhistoire-du-design-graphique.mdx&astroContentModuleFlag=true")]]);
 		
