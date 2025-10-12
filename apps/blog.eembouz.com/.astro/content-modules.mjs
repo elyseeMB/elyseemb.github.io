9ea5ef9edc6091bcfc65edfe4d573d5126afd9e7
elysee <mboussaemmanuelito@gmail.com>
2025-10-12 15:17:54 +0100

    style(blog.eembouz.com) updated style

--- apps/blog.eembouz.com/.astro/content-modules.mjs
+++ apps/blog.eembouz.com/.astro/content-modules.mjs
@@ -1,9 +1,9 @@
 
 export default new Map([
+["src/data/blog/histoire-du-design-graphique.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fhistoire-du-design-graphique.mdx&astroContentModuleFlag=true")],
+["src/data/blog/qu-est-ce-que-design-pattern.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fqu-est-ce-que-design-pattern.mdx&astroContentModuleFlag=true")],
 ["src/data/blog/convolutional-neural-networks.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fconvolutional-neural-networks.mdx&astroContentModuleFlag=true")],
 ["src/data/blog/global-id.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fglobal-id.mdx&astroContentModuleFlag=true")],
 ["src/data/blog/load-balancer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fload-balancer.mdx&astroContentModuleFlag=true")],
-["src/data/blog/qu-est-ce-que-design-pattern.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fqu-est-ce-que-design-pattern.mdx&astroContentModuleFlag=true")],
-["src/data/blog/lazy-loading.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Flazy-loading.mdx&astroContentModuleFlag=true")],
-["src/data/blog/histoire-du-design-graphique.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Fhistoire-du-design-graphique.mdx&astroContentModuleFlag=true")]]);
+["src/data/blog/lazy-loading.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2Flazy-loading.mdx&astroContentModuleFlag=true")]]);
 		
