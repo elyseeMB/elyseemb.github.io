08df2fc49550a2c22e1dafab2a73d8fa5e3091cd
elysee <mboussaemmanuelito@gmail.com>
2025-11-07 00:19:12 +0100

    fix(eembouz.com): url

--- apps/eembouz.com/.astro/content-modules.mjs
+++ apps/eembouz.com/.astro/content-modules.mjs
@@ -1,6 +1,5 @@
 
 export default new Map([
 ["src/data/projects/erp-getion.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprojects%2Ferp-getion.mdx&astroContentModuleFlag=true")],
-["src/data/projects/momo-api.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprojects%2Fmomo-api.mdx&astroContentModuleFlag=true")],
-["src/data/projects/draft/momo-api.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprojects%2Fdraft%2Fmomo-api.mdx&astroContentModuleFlag=true")]]);
+["src/data/projects/erp-gestion.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprojects%2Ferp-gestion.mdx&astroContentModuleFlag=true")]]);
 		
