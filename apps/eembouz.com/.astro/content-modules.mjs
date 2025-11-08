9ca56bb946e4fa4fc33bdada9e01c69a9c48c171
elysee <mboussaemmanuelito@gmail.com>
2025-11-08 10:38:55 +0100

    feat(eembouz.com): add fallback skeleton from photogrid

--- apps/eembouz.com/.astro/content-modules.mjs
+++ apps/eembouz.com/.astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/data/projects/mico-service-shortener-serverless.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprojects%2Fmico-service-shortener-serverless.mdx&astroContentModuleFlag=true")],
 ["src/data/projects/editeur-markdown.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprojects%2Fediteur-markdown.mdx&astroContentModuleFlag=true")],
+["src/data/projects/erp-gestion.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprojects%2Ferp-gestion.mdx&astroContentModuleFlag=true")],
 ["src/data/projects/durand-construction.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprojects%2Fdurand-construction.mdx&astroContentModuleFlag=true")],
-["src/data/projects/erp-gestion.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprojects%2Ferp-gestion.mdx&astroContentModuleFlag=true")]]);
+["src/data/projects/mico-service-shortener-serverless.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fprojects%2Fmico-service-shortener-serverless.mdx&astroContentModuleFlag=true")]]);
 		
