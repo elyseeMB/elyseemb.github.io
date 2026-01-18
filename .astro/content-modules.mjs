621366dbff42f0f09b5294a0cde6f4cee7735ef9
elysee <mboussaemmanuelito@gmail.com>
2026-01-18 01:36:49 +0100

    revert: #2

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
 ["src/data/bento/github.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fbento%2Fgithub.mdx&astroContentModuleFlag=true")],
+["src/data/bento/blog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fbento%2Fblog.mdx&astroContentModuleFlag=true")],
 ["src/data/bento/linkedin.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fbento%2Flinkedin.mdx&astroContentModuleFlag=true")],
-["src/data/bento/portfolio.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fbento%2Fportfolio.mdx&astroContentModuleFlag=true")],
-["src/data/bento/blog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fbento%2Fblog.mdx&astroContentModuleFlag=true")]]);
+["src/data/bento/portfolio.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fbento%2Fportfolio.mdx&astroContentModuleFlag=true")]]);
 		
